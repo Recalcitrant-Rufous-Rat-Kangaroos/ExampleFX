568053a51ecc18d4c4bec1bdd4da2111ca2dba99
jiananli306 <33046262+jiananli306@users.noreply.github.com>
2020-01-25 19:56:43 -0500

    Haowen Wei

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,6 +18,7 @@ public class Main {
         //Initialize new scene using the root controller class we made
         RootController scene = new RootController();
         System.out.println("John");
+        System.out.println("John2");
 
         //Run launchScene method we created in the root controller class
         scene.launchScene(args);
